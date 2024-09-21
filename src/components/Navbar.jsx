6fe36bdc0c70051ed91115e7c07e5ae90729bc40
Anamika Garg <anamikagarg2906@gmail.com>
2024-09-21 17:32:49 +0530

    routing changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,13 +20,13 @@ const Navbar = () => {
         <>
             <header className="fixed z-30 Navbar-container flex items-center justify-center w-[100vw] h-[80px]">
                 <ul className="logo-sec flex items-center justify-center relative w-[30%] h-[100%]">
-                    <li><a href="/#Home">
+                    <li><a href="/">
                         <img className='h-[50px] w-auto' src="https://orflaxindia.com/wp-content/uploads/2023/08/oipl-removebg-preview-208x74.png" alt="" /> </a></li>
                 </ul>
                 <ul className="item-sec flex relative items-center justify-center w-[80%] h-[100%] gap-12 uppercase">
                     <li className='flex'><Link to="/About" className='p-4 text-base font-bold rounded-md transition-all'>About us </Link></li>
-                    <li className='flex'><Link to="Products" className='p-4 text-base font-bold rounded-md transition-all'>Products </Link></li>
-                    <li className='flex'><Link to="Contact" className='p-4 text-base font-bold rounded-md transition-all'>Contact us </Link></li>
+                    <li className='flex'><Link to="/Products" className='p-4 text-base font-bold rounded-md transition-all'>Products </Link></li>
+                    <li className='flex'><Link to="/Contact" className='p-4 text-base font-bold rounded-md transition-all'>Contact us </Link></li>
 
                 </ul>
                 <ul className="call-sec flex items-center gap-3 uppercase h-[100%] relative w-[20%]">
@@ -40,7 +40,7 @@ const Navbar = () => {
                 smallScreen ? <div className="container afterSmallScreen">
                     <nav onClick={changeHandler} className='afterSmallScreen-nav'>
                         <ul className='afterSmallScreen-ul'>
-                        <li onClick={changeHandler}> <Link to="/#Home">Home </Link> </li>
+                        <li onClick={changeHandler}> <Link to="/">Home </Link> </li>
                         <li onClick={changeHandler}> <Link to="/About">About Us </Link> </li>
                         <li onClick={changeHandler}> <Link to="Products">Products </Link> </li>
                         <li onClick={changeHandler}> <Link to="Contact">Contact Us </Link> </li>
