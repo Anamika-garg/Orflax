2b3c6f1f640ae4f2fb0d54d7c6a88a4392ce570a
Anamika Garg <anamikagarg2906@gmail.com>
2024-09-22 21:01:53 +0530

    director page height

--- src/Pages/DirectorMsg.jsx
+++ src/Pages/DirectorMsg.jsx
@@ -8,7 +8,7 @@ const DirectorMsg = () => {
   }
   return (
     <>
-      <div className="container relative top-[120px] m-auto min-h-[50vh] w-[100vw] mb-36 flex flex-col items-center justify-center gap-10">
+      <div className="container relative top-[120px] m-auto min-h-[80vh] w-[100vw] mb-36 flex flex-col items-center justify-center gap-10">
         <h1 className="text-color text-center text-5xl font-bold uppercase">Director's Message</h1>
         <p className='text-xl font-bold p-1 mx-8 my-1' >
           “Orflax India has always stood for excellence, innovation, and integrity. Since our inception, we have embarked on a journey of growth, driven by a relentless pursuit of quality and customer satisfaction. Today, I am proud to say that we have not only met but exceeded our goals, becoming a trusted name in the field of wires and cables manufacturing.”
