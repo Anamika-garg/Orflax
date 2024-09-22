980ad4e8ce1549cd064e068a89d161d6ba65abe8
Anamika Garg <anamikagarg2906@gmail.com>
2024-09-22 09:12:23 +0530

    chatbot added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,27 +2,35 @@ import React, { useEffect, useState } from 'react'
 
 
 const Home = () => {
-  
+  const [chat, setChat] = useState(false);
+  const changeVisibility =() =>{
+    console.log(chat)
+    setChat(!chat);
+  }
+
   return (
     <div id='Home' className='Home-container min-h-[30vh] w-[100vw] m-auto flex flex-col items-center'>
       {/* carausols */}
-      <div className="carausels-con flex-col gap-10 mb-16 flex items-center justify-center relative h-[500px] w-[100%] rounded-md">
+      {
 
+          chat ?  <div className='chat-chat flex fixed gap-3 flex-col z-20 h-[50%] right-[10px] top-[200px]'><iframe
+        src="https://www.chatbase.co/chatbot-iframe/xflJLbzn1KAxo6U9lL9ry"
+        width="100%"
+        style={{ height: '60%', minHeight: '400px' }}
+        title="Chatbot"
+      ></iframe></div> : <></>
+      }
+     
+      <div className="carausels-con flex-col gap-10 mb-16 flex items-center justify-center relative h-[500px] w-[100%] rounded-md"></div>
 
-        {/* <h2 className="text-black font-semibold text-4xl">
-          World's Leading Cable Wire Industry
-        </h2>
-        <h1 className="text-color font-bold uppercase text-6xl">
-          Orflax
-        </h1>
-        <div className="btn call-now h-[50px] w-[150px] text-white font-semibold flex items-center justify-center cursor-pointer rounded-md">
-          <a href="">Call Now</a>
-        </div> */}
+      <div className="z-10 chatbot-con flex items-center justify-center mr-10 flex-col bottom-[10px] self-end fixed h-[150px] w-[150px]">
+        <div className="chatbot h-[120px] w-[120px] rounded-full relative bg-white">
+          <img src="/chatbot.png" alt="" onClick={changeVisibility} />
+          
+        </div>
+        <div className="text text-center text-color ml-10 text-lg font-bold">Need Help?</div>
       </div>
-
-      {/* <div className="sideBar h-[220px] w-[70px] bg-blue-800"></div> */}
-
-
+      
     </div>
   )
 }
